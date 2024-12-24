0767144e89dc6e3a6c866c705236aede106dcae4
Bruno <nuevobruno@outlook.com>
2024-12-24 00:11:59 -0300

    Correcciones hechas

--- js/animations.js
+++ js/animations.js
@@ -66,20 +66,3 @@ button.addEventListener('click', function(){
 })
 
 
-// Dark & Light toggle
-
-document.querySelector(".day-night input").addEventListener("change", () => {
-    document.querySelector("body").classList.add("toggle");
-    setTimeout(() => {
-      document.querySelector("body").classList.toggle("light");
-  
-      setTimeout(
-        () => document.querySelector("body").classList.remove("toggle"),
-        10
-      );
-    }, 5);
-  });
-
-
-
-
